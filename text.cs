160e20c1227a832ce0cfbeefc24594898d42d853
nicole cohen <njcohen8018@gmail.com>
2020-01-16 17:41:06 -0500

    hello world

---
+++ text.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class text : MonoBehaviour
+{
+    public Text newText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(Keycode.Space))
+        {
+            newText.text = "Hello, my name is Nicole Cohen!!!";
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
